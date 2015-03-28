f28f75a720f062557f233e617f664dc0980eac8e
Anirudh <jayakumar.anirudh@gmail.com>
2015-03-28 14:04:46 -0500

    added new vaiable to PCB structure

--- structure.h
+++ structure.h
@@ -3,39 +3,40 @@
 
 #include <linux/types.h>
 #include <linux/sched.h>
+
+/*
 enum States {
 	SLEEPING,	// 0
 	READY,		// 1
 	RUNNING,	// 2
 };
-
+*/
 typedef unsigned long ulong;
 
 
 typedef struct process_entry {
 	/* Data Structure elements */		/* Data Strcuture element explnation */
 	int pid;				// Process ID
-	ulong period;				// Period of the process
-	ulong computation;			// Computation time of process
-	ulong c;                // Question: What is this for?
-	enum States state;			// States of operation of the process coming from enum
-	struct sched_param sparam;		// Scheduling parameter of the process
 	struct timer_list mytimer;		// Timer for wakingup
 	struct task_struct *task;		// Linux task pointer
 	struct list_head list;
+	unsigned long proc_utilization;
+	unsigned long minor_fault_count;
+	unsigned long major_fault_count;
 } my_process_entry;
 
 typedef struct proc_dir_entry procfs_entry;
 
 procfs_entry* proc_filesys_entries(char *procname, char *parent);
 
-/* Admission Control Function Declaration */
-int admission_control (my_process_entry *new_process_entry);
+
+/*dd Admission Control Function Declaration */
+//int admission_control (my_process_entry *new_process_entry);
 /* Remove task function declaration used when a process is deregistering */
-int remove_task(pid_t pid);
+//int remove_task(pid_t pid);
 /* mytimer call back interface */
-void mytimer_callback(ulong data);
+//void mytimer_callback(ulong data);
 /* Worker thread to preempt the lower priority process and to execute the higher priority task */
-int workthread(void *data);
+//int workthread(void *data);
 
 #endif // STRUCTURE_H
