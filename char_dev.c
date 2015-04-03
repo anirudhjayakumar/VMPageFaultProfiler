9301ca573196a4e5d9ee689a384a7d9179fe2800
Divya Balakrishna <balakrishnadivya@gmail.com>
2015-04-03 15:36:26 -0500

    mmap implementation

--- char_dev.c
+++ char_dev.c
@@ -6,8 +6,10 @@
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/cdev.h>
+#include <linux/vmalloc.h>
+#include <linux/mm.h>
+#include "mem.h"
 
-int SUCCESS = 0, FAILURE = -1;
 static dev_t dev_num;	//variable for device number
 static struct class *c; //variable for the device class
 static struct cdev c_dev; //variable for the character device structure
@@ -22,8 +24,28 @@ static int close(struct inode *i, struct file *f)
 	return SUCCESS;
 }
 
-static int mmap(struct file *fp, struct vm_area_struct *vma)
+static int mmap_callback(struct file *fp, struct vm_area_struct *vma)
 {
+        int ret;
+        long length = vma->vm_end - vma->vm_start;
+        unsigned long start = vma->vm_start;
+        char *vmalloc_area_ptr = (char *)mm_get_buffer();
+        unsigned long pfn;
+
+        if (length > 128 * PAGE_SIZE)
+                return FAIL;
+
+        while (length > 0) {
+                pfn = vmalloc_to_pfn(vmalloc_area_ptr);
+                if ((ret = remap_pfn_range(vma, start, pfn, PAGE_SIZE,
+                                           PAGE_SHARED)) < 0) {
+                        return FAIL;
+                }
+                start += PAGE_SIZE;
+                vmalloc_area_ptr += PAGE_SIZE;
+                length -= PAGE_SIZE;
+        }
+        
 	return SUCCESS;
 }
 
@@ -32,8 +54,9 @@ static struct file_operations fops =
 {
 	.open = open,
 	.release = close,
-	.mmap = mmap 
+	.mmap = mmap_callback
 };
+
 
 int cd_initialize(void)
 {
@@ -41,20 +64,20 @@ int cd_initialize(void)
 	int ret = alloc_chrdev_region(&dev_num,0,1,"mp3");
 	if(ret != 0){
 		printk(KERN_INFO "Error creating character device file");
-		return FAILURE;
+		return FAIL;
 	}
 	printk(KERN_INFO "character device created ");	
 
 	//creating device class
 	if((c = class_create(THIS_MODULE, "mp3_class")) == NULL){
 		printk(KERN_INFO "Error creating character device class");
-		return FAILURE;
+		return FAIL;
 	}
 
 	//creating device file "node" for character device
 	if (device_create(c, NULL, dev_num, NULL, "node") == NULL){
 		printk(KERN_INFO "Error creating character device class");
-		return FAILURE;	
+		return FAIL;	
 	}
 	
 	//initializing the character device structure
@@ -63,7 +86,7 @@ int cd_initialize(void)
 	//adding character device to the system
 	if (cdev_add(&c_dev, dev_num, 1) == -1){
 		printk(KERN_INFO "Error adding character device");
-		return FAILURE;
+		return FAIL;
 	}
 	return SUCCESS;
 }
